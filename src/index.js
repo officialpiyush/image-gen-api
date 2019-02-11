b84cbf12dc1c1b121c5c3407dec604b8fe86733d
Piyush Bhangale <bhangalepiyush@gmail.com>
2019-02-11 20:59:27 +0530

    Frontend

--- src/index.js
+++ src/index.js
@@ -8,8 +8,18 @@ const bodyParser = require('body-parser');
 app.use(cors());
 app.use(bodyParser.json());
 
+/* Functions */
+function status(type, msg, res) {
+    return res.json({sucess: type, message: msg})
+}
+
 /* Endpoints */
 
 app.get('/image/achievement', async (req, res) => {
-    console.log(req, res);
+    if(!req.body.text) return status(false, 'No text Provided', res);
+    const achievement = require('./backend/generate/achievement');
+    const so = await achievement(req.body.text);
+    if(!so) return status(false, 'Text More Than 50 Chars!', res);
+    
+    return status(true, so, res);
 })
