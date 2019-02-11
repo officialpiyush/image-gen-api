026c462eec29cc6c20355842f1f3aaa669026a63
Piyush Bhangale <bhangalepiyush@gmail.com>
2019-02-11 20:38:19 +0530

    Another Endpoint

---
+++ src/backend/generate/contrast.js
@@ -0,0 +1,38 @@
+/* Import Packages */
+const {
+    createCanvas,
+    loadImage
+} = require('canvas');
+const request = require('node-superfetch');
+const {
+    contrast
+} = require('../util/CanvasUtil');
+
+/* Export Function */
+module.exports = async function ContrastEndpoint(image) {
+    try {
+        const {
+            body
+        } = await request(image);
+        const data = await loadImage(body);
+        const canvas = createCanvas(data.width, data.height);
+        const ctx = canvas.getContext('2d');
+        ctx.drawImage(data, 0, 0);
+        contrast(ctx, 0, 0, data.width, data.height);
+        const resimage = canvas.toBuffer();
+        if (Buffer.byteLength(resimage) > 8e+6) return [
+            false,
+            'Resulting image was above 8 MB'
+        ];
+        
+        return [
+            true,
+            resimage
+        ];
+    } catch (err) {
+        return [
+            false,
+            err.message
+        ]
+    }
+}
--- src/backend/util/CanvasUtil.js
+++ src/backend/util/CanvasUtil.js
@@ -7,5 +7,18 @@ module.exports = class CanvasUtil {
         }
         
 		return shorten ? `${text}...` : text;
+    }
+    static contrast(ctx, x, y, width, height) {
+		const data = ctx.getImageData(x, y, width, height);
+		const factor = (259 / 100) + 1;
+		const intercept = 128 * (1 - factor);
+		for (let i = 0; i < data.data.length; i += 4) {
+			data.data[i] = (data.data[i] * factor) + intercept;
+			data.data[i + 1] = (data.data[i + 1] * factor) + intercept;
+			data.data[i + 2] = (data.data[i + 2] * factor) + intercept;
+		}
+        ctx.putImageData(data, x, y);
+        
+		return ctx;
 	}
 }
