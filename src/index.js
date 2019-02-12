7dd8b3f2104f0e8a71ecd1ebabef1d50258e5ca3
Piyush Bhangale <bhangalepiyush@gmail.com>
2019-02-12 19:10:45 +0530

    uff

--- src/index.js
+++ src/index.js
@@ -28,6 +28,6 @@ app.get('/image/achievement', async (req, res) => {
     return status(true, so, res);
 });
 
-app.listen('3000', () => {
+app.listen(process.env.port || '3000', () => { //eslint-disable-line
     console.log("[CONNECTION] Sucessfully Connected.")
 }); 
