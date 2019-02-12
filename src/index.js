3174ca8077d4590d6c02a5f803825c265b787035
Piyush Bhangale <bhangalepiyush@gmail.com>
2019-02-12 14:01:07 +0530

    Add Body parser URL Encoding

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 
 /* Initialize Middleware*/
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 /* Functions */
