2ee22644d052f84b410e4e6ecd58873e70419d30
ExTrEmE HeRo <38991749+Anish-Shobith@users.noreply.github.com>
2019-02-12 18:32:49 +0530

    Added more Canvas Stuff And Added Extra things to README (#1)
    * Added more Canvas Stuff
    
    * Added Travis to README
    
    * Added some more Stuff to README
    
    * Improvements
    
    * Breaking Changes Soon

--- README.md
+++ README.md
@@ -12,6 +12,20 @@
   <a href="https://github.com/ionadev/image-gen-api/blob/master/LICENSE">
     <img src="https://img.shields.io/github/license/ionadev/image-gen-api.svg?style=for-the-badge" alt="Apache-2.0 License">
   </a>
+<a href="https://travis-ci.com/ionadev/image-gen-api">
+<img src="https://img.shields.io/travis/com/ionadev/image-gen-api.svg?style=for-the-badge">
+</a>
+<br>
+<a href="https://github.com/ionadev/image-gen-api">
+<img src="https://img.shields.io/github/languages/top/ionadev/image-gen-api.svg?style=for-the-badge">
+</a>
+<a href="https://github.com/ionadev/image-gen-api/issues">
+<img src="https://img.shields.io/github/issues/ionadev/image-gen-api.svg?style=for-the-badge">
+</a>
+<a href="https://github.com/ionadev/image-gen-api/pulls">
+<img src="https://img.shields.io/github/issues-pr/ionadev/image-gen-api.svg?style=for-the-badge">
+</a>
+
 </div>
 
 ---
--- src/backend/util/CanvasUtil.js
+++ src/backend/util/CanvasUtil.js
@@ -21,4 +21,79 @@ module.exports = class CanvasUtil {
         
 		return ctx;
 	}
+
+	static greyscale(ctx, x, y, width, height) {
+		const data = ctx.getImageData(x, y, width, height);
+		for (let i = 0; i < data.data.length; i += 4) {
+			const brightness = (0.34 * data.data[i]) + (0.5 * data.data[i + 1]) + (0.16 * data.data[i + 2]);
+			data.data[i] = brightness;
+			data.data[i + 1] = brightness;
+			data.data[i + 2] = brightness;
+		}
+		ctx.putImageData(data, x, y);
+		return ctx;
+	}
+
+	static invert(ctx, x, y, width, height) {
+		const data = ctx.getImageData(x, y, width, height);
+		for (let i = 0; i < data.data.length; i += 4) {
+			data.data[i] = 255 - data.data[i];
+			data.data[i + 1] = 255 - data.data[i + 1];
+			data.data[i + 2] = 255 - data.data[i + 2];
+		}
+		ctx.putImageData(data, x, y);
+		return ctx;
+	}
+
+	static silhouette(ctx, x, y, width, height) {
+		const data = ctx.getImageData(x, y, width, height);
+		for (let i = 0; i < data.data.length; i += 4) {
+			data.data[i] = 0;
+			data.data[i + 1] = 0;
+			data.data[i + 2] = 0;
+		}
+		ctx.putImageData(data, x, y);
+		return ctx;
+	}
+
+	static sepia(ctx, x, y, width, height) {
+		const data = ctx.getImageData(x, y, width, height);
+		for (let i = 0; i < data.data.length; i += 4) {
+			const brightness = (0.34 * data.data[i]) + (0.5 * data.data[i + 1]) + (0.16 * data.data[i + 2]);
+			data.data[i] = brightness + 100;
+			data.data[i + 1] = brightness + 50;
+			data.data[i + 2] = brightness;
+		}
+		ctx.putImageData(data, x, y);
+		return ctx;
+	}
+	
+		static distort(ctx, amplitude, x, y, width, height, strideLevel = 4) {
+		const data = ctx.getImageData(x, y, width, height);
+		const temp = ctx.getImageData(x, y, width, height);
+		const stride = width * strideLevel;
+		for (let i = 0; i < width; i++) {
+			for (let j = 0; j < height; j++) {
+				const xs = Math.round(amplitude * Math.sin(2 * Math.PI * 3 * (j / height)));
+				const ys = Math.round(amplitude * Math.cos(2 * Math.PI * 3 * (i / width)));
+				const dest = (j * stride) + (i * strideLevel);
+				const src = ((j + ys) * stride) + ((i + xs) * strideLevel);
+				data.data[dest] = temp.data[src];
+				data.data[dest + 1] = temp.data[src + 1];
+				data.data[dest + 2] = temp.data[src + 2];
+			}
+		}
+		ctx.putImageData(data, x, y);
+		return ctx;
+	}
+
+	static drawImageWithTint(ctx, image, color, x, y, width, height) {
+		const { fillStyle, globalAlpha } = ctx;
+		ctx.fillStyle = color;
+		ctx.drawImage(image, x, y, width, height);
+		ctx.globalAlpha = 0.5;
+		ctx.fillRect(x, y, width, height);
+		ctx.fillStyle = fillStyle;
+		ctx.globalAlpha = globalAlpha;
+	}
 }
