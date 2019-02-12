b1283331244ab4db7d22ee76e4791ceddf007f13
Piyush Bhangale <bhangalepiyush@gmail.com>
2019-02-12 18:48:55 +0530

    Changes

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+/* Import All Endpoints */
+/* TODO- V2 Class Based Endpoints */ // eslint-disable-line
+const AchievementEndpoint = require('./backend/generate/achievement');
+
 /* Functions */
 function status(type, msg, res) {
     return res.json({sucess: type, message: msg})
@@ -18,13 +22,12 @@ function status(type, msg, res) {
 
 app.get('/image/achievement', async (req, res) => {
     if(!req.body.text) return status(false, 'No text Provided', res);
-    const achievement = require('./backend/generate/achievement');
-    const so = await achievement(req.body.text);
+    const so = await AchievementEndpoint(req.body.text);
     if(!so) return status(false, 'Text More Than 50 Chars!', res);
     
     return status(true, so, res);
 });
 
 app.listen('3000', () => {
-    console.log("Connected")
+    console.log("[CONNECTION] Sucessfully Connected.")
 }); 
