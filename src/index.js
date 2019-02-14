217c99e5a65035c951a265c3300af4f897a5016f
Piyush <bhangalepiyush@gmail.com>
2019-02-14 21:10:48 +0530

    oipais

--- src/index.js
+++ src/index.js
@@ -12,10 +12,18 @@ app.use(bodyParser.json());
 /* Import All Endpoints */
 /* TODO- V2 Class Based Endpoints */ // eslint-disable-line
 const AchievementEndpoint = require('./backend/generate/achievement');
+const ApprovedEndpoint = require('./backend/generate/approved');
 
 /* Functions */
 function status(type, msg, res) {
     return res.json({sucess: type, message: msg})
+}
+
+function isImage(link) {
+    let type = link.substring(link.lastIndexOf('.') + 1)
+    let isImage = /(png)/gi.test(type);
+    if(!isImage) return false;
+    return true
 }
 
 /* Endpoints */
@@ -27,6 +35,13 @@ app.get('/image/achievement', async (req, res) => {
     
     return status(true, so, res);
 });
+
+app.get('/image/approved', async (req, res) => {
+    if(!req.body.url) return status(false, 'No URL Provided', res);
+    let check = await isImage(req.body.url);
+    if(!check) return status(false, 'The Image Isnt Of The PNG Type.');
+
+})
 
 app.listen(process.env.port || '3000', () => { //eslint-disable-line
     console.log("[CONNECTION] Sucessfully Connected.")
