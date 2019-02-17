c9b54edcbcd350a312d0338c1721a652cec1f98b
ExTrEmE HeRo <38991749+Anish-Shobith@users.noreply.github.com>
2019-02-17 19:07:25 +0530

    Added two new Endpoints (#3)
    Improvements - Anish

---
+++ src/backend/generate/invert.js
@@ -0,0 +1,38 @@
+/* Import Packages */
+const {
+    createCanvas,
+    loadImage
+} = require('canvas');
+const request = require('node-superfetch');
+const {
+    invert
+} = require('../util/CanvasUtil');
+
+/* Export Function */
+module.exports = async function InvertEndpoint(image) {
+    try {
+        const {
+            body
+        } = await request(image);
+        const data = await loadImage(body);
+        const canvas = createCanvas(data.width, data.height);
+        const ctx = canvas.getContext('2d');
+        ctx.drawImage(data, 0, 0);
+        invert(ctx, 0, 0, data.width, data.height);
+        const resimage = canvas.toBuffer();
+        if (Buffer.byteLength(resimage) > 8e+6) return [
+            false,
+            'Resulting image was above 8 MB'
+        ];
+        
+        return [
+            true,
+            resimage
+        ];
+    } catch (err) {
+        return [
+            false,
+            err.message
+        ]
+    }
+}
