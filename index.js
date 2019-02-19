1eb5060f962f77e319f3c0bdd9042703273e5840
ExTrEmE HeRo <38991749+Anish-Shobith@users.noreply.github.com>
2019-02-19 20:17:24 +0530

    lol wrong file

--- index.js
+++ index.js
@@ -11,18 +11,55 @@ app.use(bodyParser.json());
 
 /* Import All Endpoints */
 /* TODO- V2 Class Based Endpoints */ // eslint-disable-line
+/**
+ * @name AchievementEndpoint
+ * @returns {Promise}
+ */
 const AchievementEndpoint = require('./backend/generate/achievement');
+/**
+ * @name ApprovedEndpoint
+ * @returns {Promise}
+ */
 const ApprovedEndpoint = require('./backend/generate/approved');
+/**
+ * @name ContrastEndpoint
+ * @returns {Promise}
+ */
+const ContrastEndpoint = require('./backend/generate/contrast');
+/**
+ * @name FrameEndpoint
+ * @returns {Promise}
+ */
+const FrameEndpoint = require('./backend/generate/frame');
+/**
+* @name InvertEndpoint
+* @returns {Promise}
+*/
+const InvertEndpoint = require('./backend/generate/invert');
+/**
+ * @name SepiaEndpoint
+ * @returns {Promise}
+ */
+const SepiaEndpoint = require('./backend/generate/sepia');
+/**
+ * @name RejectedEndpoint
+ * @returns {Promise}
+ */
+const RejectedEndpoint = require('./backend/generate/rejected');
+
 
 /* Functions */
 function status(type, msg, res) {
+    if(type === false) res.status(400);
+
     return res.json({sucess: type, message: msg})
 }
 
 function isImage(link) {
     let type = link.substring(link.lastIndexOf('.') + 1)
-    let isImage = /(png)/gi.test(type);
-    if(!isImage) return false;
+    let isPNG = /(png)/gi.test(type); //eslint-disable-line
+    if(!isPNG) return false;
+
     return true
 }
 
@@ -34,14 +71,63 @@ app.get('/image/achievement', async (req, res) => {
     if(!so) return status(false, 'Text More Than 50 Chars!', res);
     
     return status(true, so, res);
-});
+    });
 
 app.get('/image/approved', async (req, res) => {
     if(!req.body.url) return status(false, 'No URL Provided', res);
     let check = await isImage(req.body.url);
     if(!check) return status(false, 'The Image Isnt Of The PNG Type.');
+    let result = await ApprovedEndpoint(req.body.url);
 
-})
+    return status(result[0], result[1], res)
+    });
+
+app.get('/image/contrast', async(req, res) => {
+    if(!req.body.url) return status(false, 'No URL Provided', res);
+    let check = await isImage(req.body.url);
+    if(!check) return status(false, 'The Image Isnt Of The PNG Type.');
+    let result = await ContrastEndpoint(req.body.url);
+
+    return status(result[0], result[1], res);
+    });
+
+app.get('/image/frame', async (req, res) => {
+    if(!req.body.url) return status(false, 'No URL Provided', res);
+    let check = await isImage(req.body.url);
+    if(!check) return status(false, 'The Image Isnt Of The PNG Type.');
+    let result = await FrameEndpoint(req.body.url);
+
+    return status(result[0], result[1], res);
+    });
+
+
+app.get('/image/invert', async(req, res) => {
+    if(!req.body.url) return status(false, 'No URL Provided', res);
+    let check = await isImage(req.body.url);
+    if(!check) return status(false, 'The Image Isnt Of The PNG Type.');
+    let result = await InvertEndpoint(req.body.url);
+
+    return status(result[0], result[1], res);
+    });
+
+
+app.get('/image/sepia', async(req, res) => {
+    if(!req.body.url) return status(false, 'No URL Provided', res);
+    let check = await isImage(req.body.url);
+    if(!check) return status(false, 'The Image Isnt Of The PNG Type.');
+    let result = await SepiaEndpoint(req.body.url);
+
+    return status(result[0], result[1], res);
+    });
+
+app.get('/image/rejected', async(req, res) => {
+        if(!req.body.url) return status(false, 'No URL Provided', res);
+        let check = await isImage(req.body.url);
+        if(!check) return status(false, 'The Image Isnt Of The PNG Type.');
+        let result = await RejectedEndpoint(req.body.url);
+    
+        return status(result[0], result[1], res);
+        });
 
 app.listen(process.env.port || '3000', () => { //eslint-disable-line
     console.log("[CONNECTION] Sucessfully Connected.")
